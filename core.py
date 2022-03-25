5579dfe55251675bfe6dfcef56ad4f92c4a0c3df
王懋 <oliver@aidriven.com.au>
2022-03-25 15:20:59 +0800

    bug修复

--- core.py
+++ core.py
@@ -26,39 +26,42 @@ class BaseSection(object):
         if allow_undefined is not None:
             self.allow_undefined_options = allow_undefined
 
-    def __getattr__(self, item):
+    def __getattr__(self, key, value):
         """
         未定义的属性通过注解类型获取配置的值
         """
-        print(item)
-        section_name = self.__class__.__name__
         options = self.__annotations__
         option_keys = options.keys()
-        if item not in option_keys:
+        if key not in option_keys:
             if self.allow_undefined_options:
                 # build undefined option
-                options[item] = str
+                options[key] = str
             else:
                 raise AttributeError(
-                    "No option '%s' defined in section %s." % (item, section_name)
+                    "No option '%s' defined in section %s." % (key, self._section_name)
                 )
         for option, type_val in options.items():
-            if item == option:
-                value = self._parser.get(section_name, option)
+            if key == option:
                 if type_val is bool:
-                    return str2bool(value)
+                    value = str2bool(value)
                 if type_val is list:
-                    return value.split(',')
+                    value = value.split(',')
                 if value is None:
-                    return value
-                return type_val.__call__(value)
-        return super().__getattribute__(item)
+                    value = value
+                value = type_val.__call__(value)
+        return value
 
-    def __getattribute__(self, item):
-        if item.startswith('_'):
+    def __getattribute__(self, key):
+        if key.startswith('_'):
             # 以下划线开头的属性直接返回
-            return object.__getattribute__(self, item)
-        return self.__getattr__(item)
+            return object.__getattribute__(self, key)
+        try:
+            value = self._parser.get(self._section_name, key)
+        except NoOptionError:
+            # 无法获取到配置，尝试返回属性默认值
+            return object.__getattribute__(self, key)
+        # 若存在配置，则尝试匹配注解
+        return self.__getattr__(key, value)
 
     def __new__(cls, *args, **kwargs):
         """
@@ -68,10 +71,10 @@ class BaseSection(object):
         # 没有注解的需要添加
         if not hasattr(cls, '__annotations__'):
             setattr(cls, '__annotations__', {})
-        supported_types = (str, int, float, bool, list)
+        _supported_types = (str, int, float, bool, list)
         for attr, val in cls.__annotations__.items():
-            if val not in supported_types:
-                _types = ', '.join(map(lambda t: t.__name__, supported_types))
+            if val not in _supported_types:
+                _types = ', '.join(map(lambda t: t.__name__, _supported_types))
                 raise AttributeError(
                     f"The type '{val}' for '{attr}' is not supported by settings. "
                     f"Supported types are {_types}."
